2764298e8554e5dbac991a2c70042fe9fc67c4c1
wjsgus95 <wjsgus95@gmail.com>
2023-07-16 00:29:19 +0900

    setup

--- apps/server/src/routers/user.py
+++ apps/server/src/routers/user.py
@@ -33,8 +33,6 @@ async def naver_user(access_token: str):
 
 @router.post("/register")
 async def naver_register(request: NaverUserRequest = Body()):
-    if False:
-        print("unreachable")
     naver_user = naver_client.get_user(request.access_token)
     user = User(naver_user=naver_user)
     user_service.register(user)  
